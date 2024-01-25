e452e20d764bbcd0bf61a57af14bce9657af7f80
ArturMichalak <arturmichalak96@gmail.com>
2024-01-25 02:30:02 +0100

    filled state on init

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -11,6 +11,7 @@ import React, { createElement, forwardRef, useState } from "react";
 
 const Input = forwardRef(function Input(
   {
+    value,
     type,
     label,
     labelProps,
@@ -19,12 +20,12 @@ const Input = forwardRef(function Input(
     ...props
   }: InputHTMLAttributes<HTMLInputElement> & {
     label: LabelHTMLAttributes<HTMLLabelElement>["children"];
-    labelProps?: Omit<LabelHTMLAttributes<HTMLLabelElement>, 'className'>;
+    labelProps?: Omit<LabelHTMLAttributes<HTMLLabelElement>, "className">;
     type?: "input" | "textarea";
   },
   forwardRef: ForwardedRef<HTMLInputElement>
 ) {
-  const [filled, setFilled] = useState("");
+  const [filled, setFilled] = useState<string>((value as string) || "");
 
   const handleInput: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (onChange) onChange(e);
@@ -34,7 +35,7 @@ const Input = forwardRef(function Input(
   return (
     <label
       className={classNames(
-        "border border-t-0 rounded px-2 pb-1 relative inline-block",
+        "border border-t-0 rounded px-2 py-1 relative inline-block",
         className
       )}
       {...labelProps}
@@ -42,17 +43,18 @@ const Input = forwardRef(function Input(
       {createElement(type || "input", {
         onChange: handleInput,
         ref: forwardRef,
+        value,
         className: "outline-none peer overflow-auto",
         ...props,
       })}
       <span
         className={classNames(
-          "peer-focus-within:[&>strong]:-mt-3 border-t peer-focus-within:text-sm peer-focus-within:border-t-0 rounded left-0 top-0 bottom-0 w-full absolute flex before:min-w-2 peer-focus-within:before:border-t before:rounded peer-focus-within:after:border-t after:flex-grow after:rounded pointer-events-none",
+          "peer-focus-within:[&>span]:-mt-4 border-t peer-focus-within:text-sm peer-focus-within:border-t-0 rounded left-0 top-0 bottom-0 w-full absolute flex before:min-w-2 peer-focus-within:before:border-t before:rounded peer-focus-within:after:border-t after:flex-grow after:rounded pointer-events-none",
           filled.length &&
-            "[&>strong]:-mt-3 text-sm border-t-0 before:border-t after:border-t"
+            "[&>span]:-mt-4 text-sm border-t-0 before:border-t after:border-t"
         )}
       >
-        <strong className="transition-text-and-margin">{label}</strong>
+        <span className="transition-text-and-margin leading-8">{label}</span>
       </span>
     </label>
   );
