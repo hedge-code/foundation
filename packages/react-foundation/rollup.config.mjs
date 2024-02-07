eae490e378c433d6d563b14a0d386b3d90a37a57
ArturMichalak <arturmichalak96@gmail.com>
2024-02-07 20:01:34 +0100

    types bundle

--- packages/react-foundation/rollup.config.mjs
+++ packages/react-foundation/rollup.config.mjs
@@ -2,28 +2,34 @@ import preserveDirectives from "rollup-preserve-directives";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import swc from "rollup-plugin-swc3";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/bundle.umd.js",
-      format: "umd",
-      name: "@hedgecode/foundation",
-      globals: {
-        react: "React",
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/bundle.umd.js",
+        format: "umd",
+        name: "@hedgecode/foundation",
+        globals: {
+          react: "React",
+        },
       },
+      {
+        file: "dist/bundle.cjs.js",
+        format: "cjs",
+      },
+    ],
+    plugins: [preserveDirectives(), nodeResolve(), commonjs(), swc()],
+    external: ["react"],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/bundle.d.ts",
+      format: "es",
     },
-    {
-      file: "dist/bundle.cjs.js",
-      format: "cjs",
-    },
-  ],
-  plugins: [
-    preserveDirectives(),
-    nodeResolve(),
-    commonjs(),
-    swc(),
-  ],
-  external: ["react"],
-};
+    plugins: [dts()],
+  },
+];
