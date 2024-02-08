c75a98df84603b1f86952f42c96ab282704a2493
ArturMichalak <arturmichalak96@gmail.com>
2024-02-08 14:52:49 +0100

    change d.ts export function

--- packages/react-foundation/rollup.config.mjs
+++ packages/react-foundation/rollup.config.mjs
@@ -1,32 +1,31 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import preserveDirectives from "rollup-preserve-directives";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import swc from "rollup-plugin-swc3";
-import dts from "rollup-plugin-dts";
+import ts from "rollup-plugin-ts";
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: "dist/bundle.es.js",
-        format: 'esm'
+        file: "dist/index.mjs",
+        format: "esm",
       },
       {
-        file: "dist/bundle.cjs.js",
+        file: "dist/index.cjs",
         format: "cjs",
       },
     ],
-    plugins: [peerDepsExternal(), preserveDirectives(), nodeResolve(), commonjs(), swc()],
+    plugins: [
+      preserveDirectives(),
+      peerDepsExternal(),
+      nodeResolve(),
+      commonjs(),
+      ts(),
+      swc(),
+    ],
     external: ["react", "react-dom", "uuid", "classnames"],
-  },
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/bundle.d.ts",
-      format: "es",
-    },
-    plugins: [dts()],
   },
 ];
